31b62bf5c4968d5b581c2595bb910408b5f7991d
thanhzanh <danhthanhvo23072003@gmail.com>
2024-08-01 15:13:33 +0700

    làm giao diện danh sách sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,10 +1,15 @@
-const dashboardRoutes = require('./dashboard.route');
 const systemConfig = require('../../config/system');
+
+const dashboardRoutes = require('./dashboard.route');
+
+const productRoutes = require('./product.route');
 
 module.exports = (app) => {
     //Lấy đường dẫn admin bên config/system
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
     app.use(PATH_ADMIN + '/dashboard', dashboardRoutes);
+
+    app.use(PATH_ADMIN + '/products', productRoutes);
 
 }

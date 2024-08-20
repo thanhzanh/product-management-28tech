eff932f676c2e7022fc616474f0c4ab9bfe55e14
thanhzanh <danhthanhvo23072003@gmail.com>
2024-08-20 23:30:44 +0700

    Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -18,3 +18,28 @@ module.exports.index =  async (req, res) => {
         products: products
     });
 }
+
+// [GET] /products/:slug
+
+module.exports.detail =  async (req, res) => {
+
+    try {
+        const find = {
+            deleted: false,
+            slug: req.params.slug,
+            status: "active"
+        }
+        
+        const product = await Product.findOne(find);
+
+        console.log(product);
+        
+    
+        res.render('./client/pages/products/detail', {
+            pageTitle: product.title,
+            product: product
+        });
+    } catch (error) {
+        res.redirect('/products');
+    }
+}

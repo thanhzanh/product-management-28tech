a02a6f8227fb13355451e50c9c402c0922837796
thanhzanh <danhthanhvo23072003@gmail.com>
2024-08-24 15:15:53 +0700

    TinyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
@@ -27,6 +28,10 @@ app.use(cookieParser('STUDSDAF23KAD7K'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // End flash
+
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End TinyMCE
 
 // Use pug
 app.set('views',`${__dirname}/views`);

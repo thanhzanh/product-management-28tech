3fdc5242ad7884e7452de881c6a9deac426c7cd2
thanhzanh <danhthanhvo23072003@gmail.com>
2024-08-29 22:21:46 +0700

    Xây dựng phần quản lý danh mục sản phẩm(Phần tăng số thứ tự)

--- helper/createTree.js
+++ helper/createTree.js
@@ -1,8 +1,11 @@
+let count = 0; // biến lưu bên ngoài hàm gọi là biến toàn cục
 const createTree = (arr, parentId = "") => {
     const tree = [];
         arr.forEach((item) => {
             if(item.parent_id === parentId) {
+                count++; // khi parentId thêm bằng thèn cha parent_id thì tăng biến đếm lên 1 để hiển thị STT tăng
                 const newItem = item;
+                newItem.index = count; // Tạo key index để gán biến count vào   
                 const children = createTree(arr, item.id);
                 if(children.length > 0) {
                     newItem.children = children;
@@ -14,6 +17,7 @@ const createTree = (arr, parentId = "") => {
 }
 
 module.exports.treeChildren = (arr, parentId = "") => {
+    count = 0;
     const tree = createTree(arr, parentId = "");
     return tree;
 }

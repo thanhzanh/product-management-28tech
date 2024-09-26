560ac94cbfb9dbb786645c947371b1ddfaab5039
thanhzanh <danhthanhvo23072003@gmail.com>
2024-09-26 23:21:46 +0700

    Hiển thị danh sách sản phẩm mới nhất(cái trước là danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,12 +9,23 @@ module.exports.index = async (req, res) => {
         featured: "1",
         deleted: false,
         status: "active"
-    }).limit(2);
+    }).limit(6);
 
-    const newProducts = productsHelper.priceNewProducts(productFeatured)
+    const newProductsFeatured = productsHelper.priceNewProducts(productFeatured)
+    // Lấy ra sản phẩm nổi bật
+
+    // Lấy ra danh sách sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active",
+    }).sort({ position: "desc" }).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
+    // Lấy ra danh sách sản phẩm mới nhất
 
     res.render('client/pages/home/index.pug', {
         pageTitle: 'Trang chủ',
-        productFeatured: newProducts
+        productFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }

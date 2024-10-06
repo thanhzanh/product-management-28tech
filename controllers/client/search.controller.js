7a7408f02b045540c82b1578c32fffaae5184b43
thanhzanh <danhthanhvo23072003@gmail.com>
2024-10-06 21:26:57 +0700

    Hiển thị kết quả số lượng sản phẩm khi tìm kiếm

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -4,6 +4,8 @@ const productHelper = require('../../helper/product');
 module.exports.index = async (req, res) => {
     const keyword = req.query.keyword;
 
+    let countProducts;
+
     // tạo 1 mảng để chứa những product tìm thấy được và trả ra giao diện list product theo keyword
     let newProducts = [];
     // Nếu có keyword
@@ -19,12 +21,20 @@ module.exports.index = async (req, res) => {
 
         // Gía của product
         newProducts = productHelper.priceNewProducts(products);
+
+        // Số lượng sản phẩm
+        countProducts = await Product.countDocuments({ 
+            title: regex,
+            deleted: false,
+            status: "active" 
+        });
         
     }
     
     res.render("client/pages/search/search.pug", {
         pageTitle: 'Kết quả tìm kiếm',
         keyword: keyword,
-        products: newProducts
+        products: newProducts,
+        countProducts: countProducts
     });
 }
